b0e2006af45515c93fe357c2527e8f3e6722f9d5
Leon Malisov <leonmalisov@gmail.com>
2019-11-29 21:37:56 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,7 +8,11 @@ def turn_count(board)
 end
 
 def current_player(board)
-  
+  if turn_count % 2 = 0
+    return "X"
+  elsif turn_count % 2 = 1
+    return "O"
+  end
 end
 
 # board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
